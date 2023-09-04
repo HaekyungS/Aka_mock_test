6be9189c3503d0d467dc75104125abc0bcae356d
HaekyungSeong <haekyungs95@gmail.com>
2023-09-04 17:57:37 +0900

    레이아웃 및 각 페이지 파일 생성

---
+++ app/Result/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>아카이로류는 토끼가 확실하다.</h1>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <Link href="/Intro">
+      <h1>아카이로류는 토끼가 맞다</h1>
+    </Link>
+  );
+}
