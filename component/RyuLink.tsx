77f0e86ac0a7b63b4a4b1b2311fff2a20e136014
HaekyungSeong <haekyungs95@gmail.com>
2023-09-05 15:40:04 +0900

    특정 주의사항에 링크 추가 부분이 있어 해당 컴포넌트 별도 제작

---
+++ component/RyuLink.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "../styles/page.module.css";
+
+export const RyuLink = () => {
+  return (
+    <li className={styles.message}>
+      모든 문제는 류님의
+      <Link
+        className={styles.link}
+        href="https://namu.wiki/w/%EC%95%84%EC%B9%B4%EC%9D%B4%EB%A1%9C%20%EB%A5%98"
+      >
+        {" "}
+        나무위키,
+      </Link>
+      <Link className={styles.link} href="https://www.twitch.tv/rh_ryu">
+        {" "}
+        생방송,{" "}
+      </Link>
+      <Link className={styles.link} href="https://www.youtube.com/@ryuch.821">
+        유튜브
+      </Link>
+      를 기반으로 출제하였습니다.
+    </li>
+  );
+};
