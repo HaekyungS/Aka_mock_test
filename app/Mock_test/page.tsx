e8db51e513f88e4a4fd57594c9c2030a4ca5b83b
HaekyungSeong <haekyungs95@gmail.com>
2023-09-05 15:40:18 +0900

    문제 부분 기본틀만

--- app/Mock_test/page.tsx
+++ app/Mock_test/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import Link from "next/link";
+import styles from "../../styles/page.module.css";
+import { useEffect, useState } from "react";
+import { number, problem } from "../../data/data";
+import { getCookies } from "cookies-next";
+
 export default function Page() {
-  return <h1>아카이로류는 토끼가 확실하다.</h1>;
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const user = decodeURIComponent(getCookies()["user"]);
+    setName(user);
+    // console.log(name);
+  });
+
+  return (
+    <div>
+      {/* 상단 고정부분 */}
+      <div className={`${styles.topBox} ${styles.flexColumnCenter}`}>
+        <div className={`${styles.topTitle} ${styles.flexRowCenter}`}>
+          <div className={styles.title}>2023년도 아카이로 류 모의고사</div>
+          <div className={styles.type}>아카용</div>
+        </div>
+        <div className={`${styles.topName} ${styles.flexRowCenter}`}>
+          <div className={styles.name}>성 명</div>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.name}>수험번호</div>
+          <div className={`${styles.numberbox} ${styles.name} ${styles.flexRowCenter}`}>
+            {number.map((n, index) => {
+              return (
+                <div key={index} className={styles.numberEA}>
+                  {n}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      {/* 문제 */}
+      <div className={`${styles.problem}`}></div>
+    </div>
+  );
 }
