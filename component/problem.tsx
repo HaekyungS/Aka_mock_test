731e9b7ffa90e12501b9bd88365d91931741a38e
HaekyungSeong <haekyungs95@gmail.com>
2023-09-05 17:00:05 +0900

    보기가 랜덤으로 나오는 컴포넌트 완성

--- component/problem.tsx
+++ component/problem.tsx
@@ -1,10 +1,30 @@
-// import { problem } from "../data/data";
+interface problems {
+  number: String;
+  problem: String;
+  ex: Array<String>;
+}
 
-export const Problem = (problem) => {
+export const Ex = (problem: problems) => {
+  const exNum = [];
+  for (let i = 0; i < problem.ex.length; i++) {
+    const random = Math.floor(Math.random() * problem.ex.length);
+    if (exNum.find((e) => e === random)) {
+      i--;
+    } else {
+      exNum.push(random);
+    }
+  }
+  console.log(exNum);
+
   return (
     <>
-      <div>{problem.key}</div>
-      <ol></ol>
+      <div>{problem.number}</div>
+      <div>{problem.problem}</div>
+      <ol>
+        {exNum.map((e, index) => {
+          return <li key={index}>{problem.ex[e]}</li>;
+        })}
+      </ol>
     </>
   );
 };
