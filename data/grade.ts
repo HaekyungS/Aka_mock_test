6788f7cc241e5c59e77851327527a634ffc37732
HaekyungSeong <haekyungs95@gmail.com>
2023-09-06 18:45:32 +0900

    성적계산 오류 정정

--- data/grade.ts
+++ data/grade.ts
@@ -2,7 +2,7 @@ export const grade = (score: number) => {
   if (score === 11) {
     return 100;
   } else {
-    const userGrade = score * 11;
+    const userGrade = Math.floor(score * (100 / 11));
     return userGrade;
   }
 };
