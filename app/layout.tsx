76d2be0f3e0867cc1efba9d733ae2c592c995cd1
HaekyungSeong <haekyungs95@gmail.com>
2023-09-06 17:13:38 +0900

    footer copyright 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import { gowunDodum } from "../styles/font";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={gowunDodum.className}>{children}</body>
+      <body className={gowunDodum.className}>
+        {children}
+        <footer>CopyrightⓒPowered by 쪼에(@dailyy_zoe) All Right Reserved</footer>
+      </body>
     </html>
   );
 }
