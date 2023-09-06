6d968310c433faf1477e7ba8e7a99531e9452c7e
HaekyungSeong <haekyungs95@gmail.com>
2023-09-06 16:50:15 +0900

    font 변경 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "../styles/global.css";
+import { gowunDodum } from "../styles/font";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={gowunDodum.className}>{children}</body>
     </html>
   );
 }
---
+++ styles/font.ts
@@ -0,0 +1,8 @@
+import { Gowun_Dodum } from "next/font/google";
+
+export const gowunDodum = Gowun_Dodum({
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  subsets: ["latin"],
+});
