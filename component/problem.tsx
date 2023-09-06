ad9e7c5cd5d4b970f27f142a733d78d96b8e1f35
HaekyungSeong <haekyungs95@gmail.com>
2023-09-06 11:27:25 +0900

    problem 컴포넌트 랜덤보기 적용까지 완

--- component/problem.tsx
+++ component/problem.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/page.module.css";
+
 interface problems {
   number: String;
   problem: String;
@@ -5,26 +8,50 @@ interface problems {
 }
 
 export const Ex = (problem: problems) => {
-  const exNum = [];
-  for (let i = 0; i < problem.ex.length; i++) {
-    const random = Math.floor(Math.random() * problem.ex.length);
-    if (exNum.find((e) => e === random)) {
-      i--;
-    } else {
-      exNum.push(random);
-    }
-  }
-  console.log(exNum);
+  const [answer, setAnswer] = useState("");
+  const [load, setLoad] = useState(false);
+  const [exNum, setExNum] = useState([]);
 
+  useEffect(() => {
+    const exNumA = [];
+
+    for (let i = 0; i < problem.ex.length; i++) {
+      const random = Math.floor(Math.random() * problem.ex.length);
+
+      if (exNumA.includes(random)) {
+        i--;
+      } else {
+        exNumA.push(random);
+      }
+    }
+    setExNum(exNumA);
+    setLoad(true);
+    // console.log("useEffect 안 랜덤숫자", exNum);
+  }, []);
+
+  // console.log("useEffect 밖 랜덤숫자", exNum);
   return (
     <>
-      <div>{problem.number}</div>
-      <div>{problem.problem}</div>
-      <ol>
-        {exNum.map((e, index) => {
-          return <li key={index}>{problem.ex[e]}</li>;
-        })}
+      <div className={styles.examNumber}>{problem.number} 문제</div>
+      <div className={styles.examTitle}>{problem.problem}</div>
+      <ol className={`${styles.exams} ${styles.flexColumnCenter}`}>
+        {load &&
+          exNum.map((e, index) => {
+            return (
+              <li
+                className={styles.examLi}
+                key={index}
+                onClick={() => {
+                  setAnswer(`${problem.ex[e]}`);
+                  console.log("선택한 답" + answer);
+                }}
+              >
+                {problem.ex[e]}
+              </li>
+            );
+          })}
       </ol>
+      <div className={styles.buttom}>다음 문제</div>
     </>
   );
 };
