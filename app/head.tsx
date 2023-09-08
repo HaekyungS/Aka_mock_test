037245d68a1d1a907cae51d42bb8afb78e5db3c4
HaekyungSeong <haekyungs95@gmail.com>
2023-09-08 10:47:31 +0900

    head title 추가적용

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export const Head = () => {
+  return (
+    <>
+      <title>2023년도 아카이로 류 모의고사</title>
+      <meta
+        name="description"
+        content="2023년도 아카이로 류님의 생일을 축하하여 제작한 모의고사 입니다."
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "../styles/global.css";
 import { gowunDodum } from "../styles/font";
+import { Head } from "./head";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head />
       <body className={gowunDodum.className}>
         {children}
         <footer>CopyrightⓒPowered by 쪼에(@dailyy_zoe) All Right Reserved</footer>
